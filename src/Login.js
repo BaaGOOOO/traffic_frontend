7df7fec294ac0d6c7a32198aa9f34a4e0df390aa
BaaGOOOO <joebagossy@gmail.com>
2025-04-26 16:32:46 +0200

    Fix: API URL to Render backend

--- src/Login.js
+++ src/Login.js
@@ -10,7 +10,7 @@ function Login({ onLogin }) {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post('http://localhost:5000/api/login', {
+      const res = await axios.post('https://traffic-backend-6wxr.onrender.com/api/login', {
         username,
         password
       });
